6acc8a99d2e49a1ebe442ea2bd9f5d7aa77881a9
Jiapengcs <jiapengkang@gmail.com>
2018-11-12 14:05:50 +0800

    add eureka client

--- src/main/java/com/jiapengcs/vcc/bsp/config/VccBspConfigApplication.java
+++ src/main/java/com/jiapengcs/vcc/bsp/config/VccBspConfigApplication.java
@@ -2,6 +2,7 @@ package com.jiapengcs.vcc.bsp.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 // 激活该应用为配置文件服务器：读取远程配置文件，转换为rest接口服务
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class VccBspConfigApplication {
 
